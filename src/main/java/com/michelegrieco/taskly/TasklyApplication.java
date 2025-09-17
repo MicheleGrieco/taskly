32ea0effe1665ed40e8aa5236413355f26386ed2
Michele Grieco <michelegrieco92@gmail.com>
2025-09-17 12:45:08 +0200

    first commit

---
+++ src/main/java/com/michelegrieco/taskly/TasklyApplication.java
@@ -0,0 +1,13 @@
+package com.michelegrieco.taskly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michelegrieco/taskly/TasklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelegrieco.taskly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
