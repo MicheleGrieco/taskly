ddf36287bc67dd4494640adb1a5ffb596c5d6bbd
Michele Grieco <michelegrieco92@gmail.com>
2025-09-18 09:35:58 +0200

    repo interface

---
+++ src/main/java/com/michelegrieco/taskly/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.michelegrieco.taskly.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.michelegrieco.taskly.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
+
+}
